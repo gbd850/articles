95fac57568504c2183ef09f048ba6bbb419ddfbf
gbd850 <mmyszaki@gmail.com>
2024-04-28 22:59:00 +0200

    Add calculating Hirsch index function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 def H_index(citations):
+    citations.sort()
+
+    for i, cited in enumerate(citations):
+
+        result = len(citations) - i
+
+        if result <= cited:
+            return result
+
+    return 0
+
 
 if __name__ == '__main__':
+    citations = [50, 40, 33, 23, 12, 11, 8, 5, 1, 0]
+    print(H_index(citations))
