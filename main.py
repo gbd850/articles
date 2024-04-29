4482df9cb2c307ec85d3e09f23e4d53962e35e67
gbd850 <mmyszaki@gmail.com>
2024-04-29 21:44:07 +0200

    Edit ministerial list evaluation function
    Edit ministerial list evaluation to accommodate for edge cases and aggregate articles into a tuple

--- main.py
+++ main.py
@@ -12,24 +12,39 @@ def h_index(citations):
 
 
 def minist_list(authors_articles):
-    points = dict()
     for author in authors_articles:
         articles = authors_articles[author]
-        if len(articles) >= 3:
-            selected_articles = dict(sorted(articles.items(), reverse=True, key=lambda item: item[1])[:3])
-            points[author] = (selected_articles, sum(selected_articles.values()))
-        elif len(articles) <= 2:
+        if len(articles) < 3:
+            for auth in authors_articles:
+                if len(authors_articles[auth]) > 3:
+                    best_article = sorted(authors_articles[auth].items(), reverse=True, key=lambda item: item[1])[0]
+                    authors_articles[author][best_article[0]] = best_article[1]
+                    del authors_articles[auth][best_article[0]]
 
+    sum_articles = list()
+    points = 0
+    for author in authors_articles:
+        articles = authors_articles[author]
+        selected_articles = sorted(articles.items(), reverse=True, key=lambda item: item[1])[:3]
+        selected_articles_names = [item[0] for item in selected_articles]
+        selected_articles_points = [item[1] for item in selected_articles]
+        sum_articles += selected_articles_names
+        points += sum(selected_articles_points)
 
+    return sum_articles, points
 
 
 if __name__ == '__main__':
     citations = [50, 40, 33, 23, 12, 11, 8, 5, 1, 0]
     print(h_index(citations))
     auth_art = dict()
-    auth_art['Author1'] = {'article1': 4,
+    auth_art['Author1'] = {'article1': 40,
                            'article2': 3,
-                           'article3': 2
+                           'article3': 2,
+                           'article4': 1
                            }
-    minist_list(auth_art)
+    auth_art['Author2'] = {'article11': 5,
+                           'article21': 1
+                           }
+    print(minist_list(auth_art))
 
