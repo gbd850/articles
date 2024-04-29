f12fd71460c1732474b19a500e903dc048fcb1f7
gbd850 <mmyszaki@gmail.com>
2024-04-29 11:10:51 +0200

    Add ministerial list base case

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def H_index(citations):
+def h_index(citations):
     citations.sort()
 
     for i, cited in enumerate(citations):
@@ -11,6 +11,25 @@ def H_index(citations):
     return 0
 
 
+def minist_list(authors_articles):
+    points = dict()
+    for author in authors_articles:
+        articles = authors_articles[author]
+        if len(articles) >= 3:
+            selected_articles = dict(sorted(articles.items(), reverse=True, key=lambda item: item[1])[:3])
+            points[author] = (selected_articles, sum(selected_articles.values()))
+        elif len(articles) <= 2:
+
+
+
+
 if __name__ == '__main__':
     citations = [50, 40, 33, 23, 12, 11, 8, 5, 1, 0]
-    print(H_index(citations))
+    print(h_index(citations))
+    auth_art = dict()
+    auth_art['Author1'] = {'article1': 4,
+                           'article2': 3,
+                           'article3': 2
+                           }
+    minist_list(auth_art)
+
